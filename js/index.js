166cc64d2874ecad99aab291cef707830faa29b9
etridgely <5757423+etridgely@users.noreply.github.com>
2019-08-06 17:05:41 -0700

    finished

--- js/index.js
+++ js/index.js
@@ -11,9 +11,14 @@ header.forEach( a => {
     })
 });
 
-const image = document.querySelector('.intro');
+const intro = document.querySelector('.intro');
+    intro.addEventListener('dblclick', (f) => {
+        intro.classList.toggle('large');
+    });
+
+const image = document.querySelector('.intro img');
     image.addEventListener('dblclick', (f) => {
-        image.classList.toggle('large');
+        f.stopPropagation();
     });
 
 const likeBtn = document.querySelector('.btn');
@@ -37,23 +42,57 @@ function zoom(event) {
 let scale = 1;
 const el = document.querySelector('footer');
 el.onwheel = zoom; 
+
+const log = document.querySelector('body');
+    window.addEventListener('load', () => {
+        console.log('page has loaded');
+    });
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+    // Do something with the scroll position
+    console.log(`scrollin dirty`);
+}
+
+window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+        doSomething(last_known_scroll_position);
+        ticking = false;
+        });
+
+        ticking = true;
+    }
+});
+
+const preventer = document.querySelectorAll('.nav-link');
+    preventer.forEach( a => {
+        a.addEventListener('click', function(event) {
+            console.log(`the Nav will not work`);
+            event.preventDefault();
+        })
+    });
   
-// const btnHover = document.querySelectorAll('.btn');
-// btnHover.forEach(btn => {
-//     btn.addEventListener('mouseover', (e) => {
-//         TweenMax.to(e.target, 1, {
-//             scale: 1.3,
-//             filter: 'none',
-//             ease: Elastic.easeOut.config( 1, 0.75)
-//         })
-//     })
-//     btn.addEventListener('mouseleave', (e) => {
-//         TweenMax.to(e.target, 0.5, {
-//             scale: 1,
-//             filter: 'grayscale(1) blur(3px)',
-//             ease: Power1.easeIn
-//         })
-//     })
-// });
+const btnHover = document.querySelectorAll('.btn');
+btnHover.forEach(btn => {
+    btn.addEventListener('mouseover', (e) => {
+        TweenMax.to(e.target, 1, {
+            scale: 1.3,
+            filter: 'none',
+            ease: Elastic.easeOut.config( 1, 0.75)
+        })
+    })
+    btn.addEventListener('mouseleave', (e) => {
+        TweenMax.to(e.target, 0.5, {
+            scale: 1,
+            filter: 'grayscale(1) blur(3px)',
+            ease: Power1.easeIn
+        })
+    })
+});
 
 
